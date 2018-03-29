6687dbc256f66c5c5ee496b7e3513730d5d95d26
Rajeev Kumar Singh <rajeevs@flock.co>
2018-03-29 20:20:21 +0530

    Using WebMvcConfigurer

--- polling-app-server/src/main/java/com/example/polls/config/WebMvcConfig.java
+++ polling-app-server/src/main/java/com/example/polls/config/WebMvcConfig.java
@@ -2,13 +2,14 @@ package com.example.polls.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by rajeevkumarsingh on 02/08/17.
  */
+
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
