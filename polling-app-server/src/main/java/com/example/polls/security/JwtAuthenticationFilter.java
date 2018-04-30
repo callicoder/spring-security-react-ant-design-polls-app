dcee575f64f9cd7ec848bcdab6e4ec74521e30c9
Rajeev Kumar Singh <rajeevs@flock.co>
2018-05-01 00:44:01 +0530

    Added comment

--- polling-app-server/src/main/java/com/example/polls/security/JwtAuthenticationFilter.java
+++ polling-app-server/src/main/java/com/example/polls/security/JwtAuthenticationFilter.java
@@ -37,6 +37,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
                 Long userId = tokenProvider.getUserIdFromJWT(jwt);
 
+                /*
+                    Note that you could also encode the user's username and roles inside JWT claims
+                    and create the UserDetails object by parsing those claims from the JWT.
+                    That would avoid the following database hit. It's completely up to you.
+                 */
                 UserDetails userDetails = customUserDetailsService.loadUserById(userId);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
