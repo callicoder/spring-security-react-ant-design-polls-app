a1a162deb1dc17faa96550ce2afe0fc0cfbb8810
Rajeev Kumar Singh <rajeevs@flock.co>
2018-07-22 18:22:56 +0530

    Added CORS config

--- polling-app-server/src/main/java/com/example/polls/config/WebMvcConfig.java
+++ polling-app-server/src/main/java/com/example/polls/config/WebMvcConfig.java
@@ -11,8 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final long MAX_AGE_SECS = 3600;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
     }
 }
