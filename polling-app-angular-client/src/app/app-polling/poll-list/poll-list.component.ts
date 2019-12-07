4e2d1bf1d52633433be8a88c719965d49597f97a
Jannie Louwrens <jannie@opencollab.co.za>
2019-12-07 18:14:12 +0200

    hide detail until data has finished loading

--- polling-app-angular-client/src/app/app-polling/poll-list/poll-list.component.ts
+++ polling-app-angular-client/src/app/app-polling/poll-list/poll-list.component.ts
@@ -8,16 +8,18 @@ import { PollInfo } from './../models/poll-info';
 @Component({
   selector: 'app-poll-list',
   template: `
-  <div class="polls-container" *ngIf="polls?.length > 0; else templateForEmpty">
-     <ng-container>
-      <poll-view *ngFor="let poll of polls" [poll]='poll'></poll-view>
-    </ng-container>
-  </div>
-  <ng-template #templateForEmpty>
-    <div class="polls-container no-polls-found">
-      <span>No Polls Found.</span>
+  <div *ngIf="!isLoading">
+    <div class="polls-container" *ngIf="polls?.length > 0; else templateForEmpty">
+      <ng-container>
+        <poll-view *ngFor="let poll of polls" [poll]='poll'></poll-view>
+      </ng-container>
     </div>
-  </ng-template>
+    <ng-template #templateForEmpty>
+      <div class="polls-container no-polls-found">
+        <span>No Polls Found.</span>
+      </div>
+    </ng-template>
+  </div>
   `,
   styles: [`
   .polls-container {
@@ -37,6 +39,7 @@ export class PollListComponent implements OnInit, OnChanges {
   @Input() public username: string;
   @Input() public type: string;
   polls: PollInfo[];
+  isLoading = false;
 
   constructor(
     private pollService: PollService,
@@ -54,6 +57,7 @@ export class PollListComponent implements OnInit, OnChanges {
   }
 
   private loadingPolls() {
+    this.isLoading = true;
     if (this.type === 'USER_CREATED_POLLS') {
       this.loadUserCreatedPolls(this.username, 0, 30);
     } else if (this.type === 'USER_VOTED_POLLS') {
@@ -67,9 +71,11 @@ export class PollListComponent implements OnInit, OnChanges {
     this.pollService.getAllPolls(page, size)
       .pipe(first())
       .subscribe(res => {
+        this.isLoading = false;
         this.polls = res.content;
       },
       error => {
+        this.isLoading = false;
         this.toastr.error(error, 'Polling App');
       });
   }
@@ -78,9 +84,11 @@ export class PollListComponent implements OnInit, OnChanges {
     this.pollService.getUserCreatedPolls(username, page, size)
       .pipe(first())
       .subscribe(res => {
+        this.isLoading = false;
         this.polls = res.content;
       },
       error => {
+        this.isLoading = false;
         this.toastr.error(error, 'Polling App');
       });
   }
@@ -89,9 +97,11 @@ export class PollListComponent implements OnInit, OnChanges {
     this.pollService.getUserVotedPolls(username, page, size)
       .pipe(first())
       .subscribe(res => {
+        this.isLoading = false;
         this.polls = res.content;
       },
       error => {
+        this.isLoading = false;
         this.toastr.error(error, 'Polling App');
       });
   }
